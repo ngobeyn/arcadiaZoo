c205563da84cb811c87c4d11d796411116fbef2b
Nicolas GOBEYN <167229806+ngobeyn@users.noreply.github.com>
2024-07-26 01:42:38 +0200

    Création de la page EditPassword

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,7 +3,9 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/signin", "Connexion", "/pages/signin.html"),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+    new Route("/account", "Mon compte", "/pages/auth/account.html"),
+    new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
