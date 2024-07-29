17bd68cc7a2e1ef17873a42efe9e25e4c14c8f54
Nicolas GOBEYN <167229806+ngobeyn@users.noreply.github.com>
2024-07-29 22:27:02 +0200

    Update Signin - Ajout des fonctionnalités JS (cookie + token)

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,7 +3,7 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html", "js/auth/signin.js"),
     new Route("/account", "Mon compte", "/pages/auth/account.html", "/js/auth/account.js"),
     new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html"),
     new Route("/editAccount", "Modifier mes informations", "/pages/auth/editAccount.html"),
---
+++ js/auth/signin.js
@@ -0,0 +1,23 @@
+const mailInput = document.getElementById("EmailInput");
+const passwordInput = document.getElementById("PasswordInput");
+const btnSignin = document.getElementById("btnSignin");
+
+btnSignin.addEventListener("click", checkCredentials);
+
+function checkCredentials(){
+    
+    // Ici, il faudra appeler l'API pour vérifier les credentials en BDD
+
+    if(mailInput.value == "test@mail.com" && passwordInput.value == "123"){
+        // Il faudra récupérer le vrai token    
+        const token = "sdkjbsdmjqspoqdshdsondôioknzOEHCSÇIO<NZPIADHC!ZEFGZgcpusch";
+        setToken(token);
+        // Placer le token en cookie
+
+        window.location.replace("/");
+    }
+    else{
+        mailInput.classList.add("is-invalid");
+        passwordInput.classList.add("is-invalid");
+    }
+}
