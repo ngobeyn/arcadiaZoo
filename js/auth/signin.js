aaaaca7126a412a138cd7f21524e27a9cf9fd9bc
Nicolas GOBEYN <167229806+ngobeyn@users.noreply.github.com>
2024-07-29 16:39:46 +0200

    Update - Signin (Ajout fonctionnalités JS)
    Ajout des fonctionnalité JS
    - Gestion Cookies & Token

--- js/auth/signin.js
+++ js/auth/signin.js
@@ -13,6 +13,7 @@ function checkCredentials(){
         // Il faudra récupérer le vrai token
         const token = "lkdslnkjneoodlsmljzejqbciubqckjbciubqckjhbqziuybzqiboib";
 
+        setToken(token);
         // Placer ce token en cookie
 
         window.location.replace("/");
