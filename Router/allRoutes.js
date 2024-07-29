3829293e1febd4f1de99b37f4f001d823c12c9a9
Nicolas GOBEYN <167229806+ngobeyn@users.noreply.github.com>
2024-07-29 18:25:34 +0200

    Update files

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,7 +4,7 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/signin", "Connexion", "/pages/auth/signin.html"),
-    new Route("/account", "Mon compte", "/pages/auth/account.html"),
+    new Route("/account", "Mon compte", "/pages/auth/account.html", "/js/auth/account.js"),
     new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html"),
     new Route("/editAccount", "Modifier mes informations", "/pages/auth/editAccount.html"),
     new Route("/signup", "Création d'un utilsateur", "/pages/auth/signup.html", "/js/auth/signup.js"),
