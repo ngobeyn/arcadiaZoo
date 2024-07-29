9b10992b4ec3acd1397e45908e3b810113c5dab5
Nicolas GOBEYN <167229806+ngobeyn@users.noreply.github.com>
2024-07-29 17:24:54 +0200

    Update Signin Files

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,15 +3,11 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-<<<<<<< Updated upstream
     new Route("/signin", "Connexion", "/pages/signin.html"),
-=======
-    new Route("/signin", "Connexion", "/pages/auth/signin.html", "/js/auth/signin.js"),
     new Route("/account", "Mon compte", "/pages/auth/account.html"),
     new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html"),
     new Route("/editAccount", "Modifier mes informations", "/pages/auth/editAccount.html"),
     new Route("/signup", "Création d'un utilsateur", "/pages/auth/signup.html", "/js/auth/signup.js"),
->>>>>>> Stashed changes
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
